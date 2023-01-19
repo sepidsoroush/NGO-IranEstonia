7a1a240f9154db7a59708971aba4c937bdc2a8d5
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-19 17:15:20 +0200

    setup default structure of pages

---
+++ src/pages/Gallery.js
@@ -0,0 +1,5 @@
+const Gallery = () => {
+    return <h1>Pictures</h1>;
+  };
+  
+  export default Gallery;
---
+++ src/pages/Membership.js
@@ -0,0 +1,5 @@
+const Membership = () => {
+    return <h1>register</h1>;
+  };
+  
+  export default Membership;
