2576575b290aad96ef0260fd40f83fc1dd79cc9c
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-21 11:55:31 +0200

    create event containers based on EventInfo

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import EventsInfo from '../data/EventsInfo';
+// import Slider from "../components/Slider" ;
 
 const Events = () => {
     return (
-        <div></div>
+        <div className='Event-container'>
+            {EventsInfo.map((item)=>{
+                const {id , title , description , image} = item;
+                return(
+                    <div className='event-item' key={id}>
+                        <h2 className='event-title'>{title}</h2>
+                        <p className='event-description'>{description}</p>
+                        <button type='button' className='event-more'>Learn More </button>
+                    </div>
+                )
+            })}
+        </div>
     );
 };
 
