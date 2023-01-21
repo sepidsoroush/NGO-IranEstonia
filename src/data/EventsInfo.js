98fe2cb13686444cadf7b9cd6e96b70d0b70c071
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-21 11:10:23 +0200

    create an array of "sample images" in EventsInfo

--- src/data/EventsInfo.js
+++ src/data/EventsInfo.js
@@ -4,9 +4,8 @@ const EventsInfo = [
       title: 'Mahsa Amini Memorial',
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.",
-  
       image:
-        'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88',
+      ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
     },
     {
       id: 2,
@@ -14,7 +13,7 @@ const EventsInfo = [
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
-        'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3',
+      ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
     },
     {
       id: 3,
@@ -22,7 +21,7 @@ const EventsInfo = [
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
-        'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372',
+      ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
     },
     {
       id: 4,
@@ -30,7 +29,7 @@ const EventsInfo = [
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
-        'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide04.jpeg?alt=media&token=8b8a146c-73f9-4e25-af22-b8fc9e71faac',
+      ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
     },
   ];
   
