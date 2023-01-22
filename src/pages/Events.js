1332ae62dbad1eb011ed61cc8343974bf623f738
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-22 09:30:43 +0200

    change buttons to Link

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import EventsInfo from '../data/EventsInfo';
-import { useNavigate } from "react-router-dom";
-
 
 const Events = () => {
-    const navigate = useNavigate();
-
     return (
         <div className='Event-container'>
             {EventsInfo.map((item)=>{
@@ -14,7 +11,7 @@ const Events = () => {
                     <div className='event-item' key={id}>
                         <h2 className='event-title'>{title}</h2>
                         <p className='event-description'>{description}</p>
-                        <button onClick={()=>navigate(`/events/${id}`)}  className='btn btn-details'>Learn more</button>
+                        <Link to={`/events/${id}`}  className='btn btn-details'>Learn more</Link>
                         
                     </div>
                 )
