8ebe13df92fdf109e2d35a520ae96e68f2b0a9a3
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 10:22:40 +0200

    create an array for team info

---
+++ src/data/TeamInfo.js
@@ -0,0 +1,33 @@
+const Team = [
+    {
+        id:1,
+        name: "Lorem Ipsum",
+        position : "co-founder",
+        image :"test"
+    },
+    {
+        id:2,
+        name: "Lorem Ipsum",
+        position : "co-founder",
+        image :"test"
+    },
+    {
+        id:3,
+        name: "Lorem Ipsum",
+        position : "Event Planner",
+        image :"test"
+    },
+    {
+        id:4,
+        name: "Lorem Ipsum",
+        position : "Board Member",
+        image :"test"
+    },
+    {
+        id:5,
+        name: "Lorem Ipsum",
+        position : "Board Member",
+        image :"test"
+    }
+];
+export default Team;
