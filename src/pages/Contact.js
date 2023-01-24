5393bcd0453e57c892e8f0731b758b0262ddd8e8
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 20:27:45 +0200

    Add Get In Touch div in Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,8 +17,16 @@ const Contact = () => {
             </li>
           </ul>
       </div>
-
-
+      <div>
+        <h1>Get In Touch</h1>
+        <p>Your email address will not be published. Required fields are marked *</p>
+        <form action="">
+          <input type="name" placeholder="Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="textarea" placeholder="Your Message" />
+          <input type="submit" value="Send Message" />
+        </form>
+      </div>
     </div>
   );
   };
