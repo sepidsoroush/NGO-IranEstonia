6654200b47271102a801cfa685300cf5be785d09
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 18:40:44 +0200

    add Hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
+import Logo from "../data/Images/Logo.jpg";
+
 const Home = () => {
-    return <h1>Home</h1>;
+    return(
+      <div>
+        <section>
+          <img style={{height : "100vh" , width : "20vw"}} src={Logo} alt="Iranian supportive organization" />
+          <h1>Iranian supportive organization</h1>
+          <Link to="/donate">Donate</Link>
+        </section>
+      </div>
+    );
   };
   
   export default Home;
