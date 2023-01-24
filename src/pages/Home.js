7fc56bc32e1d29b025ee9e2975422bf9bdc0d11a
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 20:12:49 +0200

    create section events

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Events from "./Events" ;
 
 import Logo from "../data/Images/Logo.jpg";
 
@@ -12,7 +13,9 @@ const Home = () => {
             <Link to="/donate">Donate</Link>
           </div>
         </section>
-
+        <section className="section-events">
+          <Events />
+        </section>
       </div>
     );
   };
