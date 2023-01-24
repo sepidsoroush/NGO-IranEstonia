116e1ddad52d0ce96285f6e4721a68101d4abe59
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 18:48:45 +0200

    add a div to section hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,14 @@ import Logo from "../data/Images/Logo.jpg";
 const Home = () => {
     return(
       <div>
-        <section>
-          <img style={{height : "100vh" , width : "20vw"}} src={Logo} alt="Iranian supportive organization" />
-          <h1>Iranian supportive organization</h1>
-          <Link to="/donate">Donate</Link>
+        <section className="section-hero">
+          <img style={{height : "680px" , width : "400px"}} src={Logo} alt="Iranian supportive organization" />
+          <div>
+            <h1>Iranian supportive organization</h1>
+            <Link to="/donate">Donate</Link>
+          </div>
         </section>
+
       </div>
     );
   };
