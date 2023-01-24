bb84fc7eb197da916d8a847c3271d8bd264ae344
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 17:31:28 +0200

    seprate 2 section in events

--- src/data/EventsInfo.js
+++ src/data/EventsInfo.js
@@ -6,6 +6,7 @@ const EventsInfo = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.",
       image:
       ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
+      ,type : "Past Events"
     },
     {
       id: 2,
@@ -14,6 +15,7 @@ const EventsInfo = [
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
       ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
+      ,type : "Past Events"
     },
     {
       id: 3,
@@ -22,6 +24,7 @@ const EventsInfo = [
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
       ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
+      ,type : "Past Events"
     },
     {
       id: 4,
@@ -30,6 +33,7 @@ const EventsInfo = [
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.',
       image:
       ['https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide03.jpeg?alt=media&token=d8987250-7763-4e6e-a70e-b48406232372' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide01.jpeg?alt=media&token=3c87050e-dbf9-4e5c-b20c-9aa35e33df88' , 'https://firebasestorage.googleapis.com/v0/b/ayhan-b7555.appspot.com/o/files%2Fslide02.jpeg?alt=media&token=8c038600-74a9-426e-aab2-c8e3e74d96d3' ]
+      ,type : "Upcoming Events"
     },
   ];
   
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,19 +3,36 @@ import { Link } from 'react-router-dom';
 import EventsInfo from '../data/EventsInfo';
 
 const Events = () => {
+    const Upcoming = EventsInfo.filter((item) => item.type === "Upcoming Events");
+    const Past = EventsInfo.filter((item) => item.type === "Past Events");  
     return (
         <div className='Event-container'>
-            {EventsInfo.map((item)=>{
-                const {id , title , description} = item;
-                return(
-                    <div className='event-item' key={id}>
-                        <h2 className='event-title'>{title}</h2>
-                        <p className='event-description'>{description}</p>
-                        <Link to={`/events/${id}`}  className='btn btn-details'>Learn more</Link>
-                        
-                    </div>
-                )
-            })}
+            <section>
+                <h1>Upcoming Events</h1>
+                {Upcoming.map((item)=>{
+                    const {id , title , description} = item;
+                    return(
+                        <div className='event-item' key={id}>
+                            <h2 className='event-title'>{title}</h2>
+                            <p className='event-description'>{description}</p>
+                            <Link to={`/events/${id}`}  className='btn btn-details'>Learn more</Link>
+                        </div>
+                    )
+                })}
+            </section>
+            <section>
+                <h1>Past Events</h1>
+                {Past.map((item)=>{
+                    const {id , title , description} = item;
+                    return(
+                        <div className='event-item' key={id}>
+                            <h2 className='event-title'>{title}</h2>
+                            <p className='event-description'>{description}</p>
+                            <Link to={`/events/${id}`}  className='btn btn-details'>Learn more</Link>
+                        </div>
+                    )
+                })}
+            </section>
         </div>
     );
 };
