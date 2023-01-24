c4a11a2dd932de38abb12da4b69de10e559334dd
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 10:16:12 +0200

    add more sections to About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import { UilInstagram , UilTwitter , UilFacebookF } from "@iconscout/react-unico
 const About = () => {
   return(
     <div>
-      <div>
+      <section>
         <h2>About Iranian supportive organization (ISO)</h2>
         <p>The Iranian Supportive Organization (ISO) is a non-governmental organization (NGO) that is focused on political activism and opposition to the current regime in Iran, the Islamic Republic. The organization's mission is to support the Iranian people in their struggle for freedom, democracy, and human rights.</p>
         <p>ISO was established by a group of Iranian expats who are deeply concerned about the current state of affairs in Iran, and who believe that the Islamic Republic regime is not representative of the Iranian people. Since its inception, ISO has been actively engaged in a wide range of activities and programs that aim to bring about change in Iran.</p>
@@ -10,7 +10,17 @@ const About = () => {
         <p>ISO also engages in lobbying and advocacy efforts, working with governments, international organizations, and other NGOs to put pressure on the Islamic Republic regime to respect the rights and freedoms of the Iranian people. The organization also works closely with other Iranian opposition groups and pro-democracy activists, both inside and outside of Iran.</p>
         <p>In addition to its political activities, ISO also provides support and assistance to Iranians who have been affected by the regime's repression and human rights abuses. The organization works to provide legal and humanitarian assistance to Iranians who have been arrested, imprisoned, or exiled for their political beliefs.</p>
         <p>Overall, The Iranian Supportive Organization is a vital resource for Iranians seeking change in their country. The organization's work in political activism, advocacy, and support for the pro-democracy movement are crucial in helping Iranians to achieve freedom, democracy, and human rights in their country.</p>
-      </div>
+      </section>
+      <section>
+        <h2>Our Story</h2>
+        <p>Sed porttitor lectus nibh. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
+        <p>Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Nulla porttitor accumsan tincidunt. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+        <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+      </section>
+      <section>
+        <h2>The Team</h2>
+
+      </section>
     </div>
   );
   };
