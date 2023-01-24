a4f69fb25068adca702a5dc057626dc397a1b980
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-24 20:14:37 +0200

    delete unused imports

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Team from '../data/TeamInfo';
-import { UilInstagram , UilTwitter , UilFacebookF } from "@iconscout/react-unicons";
 
 const About = () => {
   return(
