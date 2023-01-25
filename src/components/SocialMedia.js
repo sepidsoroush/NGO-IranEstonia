84727934e5e4b3b6d20cda567f248057c3292ca1
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-25 14:12:11 +0200

    Add style to social media buttons

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -3,14 +3,14 @@ import { UilInstagram , UilTwitter , UilFacebookF } from "@iconscout/react-unico
 const Media = () => {
     return(
       <div>
-            <ul>
-                <li>
-                    <a href="https://www.facebook.com/iraniansupportive"><UilFacebookF /></a>
+            <ul className="flex flex-row">
+                <li className="w-18 h-18 text-white p-2 mx-1 bg-persian-indigo-700 rounded-full ">
+                    <a href="https://www.facebook.com/iraniansupportive" ><UilFacebookF /></a>
                 </li>
-                <li>
+                <li className="w-18 h-18 text-white p-2 mx-1 bg-persian-indigo-700 rounded-full ">
                     <a href="https://www.instagram.com/freeirangroup/"><UilInstagram/></a>
                 </li>
-                <li>
+                <li className="w-18 h-18 text-white p-2 mx-1 bg-persian-indigo-700 rounded-full ">
                     <a href="https://twitter.com/iranianiso?s=21&t=Jpm22UuHWW--BpFe9r8y3A"><UilTwitter/></a>
                 </li>
             </ul>
