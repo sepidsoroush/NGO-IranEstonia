beec32a8503d44d3a4954a90322c77f101400eae
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-25 15:45:25 +0200

    delete margin left of first icon

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,7 +4,7 @@ const Media = () => {
     return(
       <div>
             <ul className="flex flex-row">
-                <li className="w-18 h-18 text-white p-2 mx-1 bg-persian-indigo-700 rounded-full ">
+                <li className="w-18 h-18 text-white p-2 mr-1 bg-persian-indigo-700 rounded-full ">
                     <a href="https://www.facebook.com/iraniansupportive" ><UilFacebookF /></a>
                 </li>
                 <li className="w-18 h-18 text-white p-2 mx-1 bg-persian-indigo-700 rounded-full ">
