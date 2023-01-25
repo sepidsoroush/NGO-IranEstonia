4e678c0944a5d0312abba8c9c411333c0c4869ad
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-25 11:48:10 +0200

    add a pallet custom color to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors :{
+        'persian-indigo' : {
+          50: '#f4f0fe' ,
+          100: '#dbcef9',
+          200: '#c2acf4',
+          300: '#a88aee',
+          400: '#7745e4',
+          500: '#5e23df',
+          600: '#40169c',
+          700: '#32127a',
+          800: '#1e0a47',
+          900: '#0f0625', 
+        }
+    }
   },
   plugins: [],
 }
