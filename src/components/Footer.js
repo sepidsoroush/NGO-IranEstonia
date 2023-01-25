57bb86f5f65f8540241b0e28d6f32dc02f08b3b8
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-25 15:33:45 +0200

    Add style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,37 +4,39 @@ import { Outlet, Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-        <div>
-            <h2>Iranian supportive organization</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc eros, accumsan at nisl ut, bibendum ornare ipsum. Donec velit.</p>
+    <div className='bg-persian-indigo-50 text-persian-indigo-700 text-sm  w-screen'>
+        <div className='flex flex-row justify-around w-3/4 mx-auto'>
+        <div className=' my-6 w-32'>
+            <p className='text-base font-bold mb-2' >Contact us</p>
+            <div >
+                <Media />
+            </div>
         </div>
-        <div>
-            <h2>Main Menu</h2>
+        <div className='my-6 w-60'>
+            <p className='text-base font-bold' >Newsletter</p>
+            <p className='text-xs text-justify my-2'>Sign up to our Newsletter, to recieve updates on the latest news and upcoming events. We respect your privacy, Your Information is safe with us.</p>
+            <form action="">
+                <input type="email" placeholder='Email Address' className=' pl-2 py-1 text-gray-500 mr-2 rounded' />
+                <input type="submit" className='bg-persian-indigo-700 px-2 py-1 rounded text-white'  />
+            </form>
+        </div>
+        <div className='my-6 w-32'>
             <ul>
-                <li>
+                <li className='mb-2'>
                     <Link to="/">Home Page</Link>
                 </li>
-                <li>
-                    <Link to="/membership">Become a member</Link>
+                <li className='my-2'>
+                    <Link to="/events">Our Events</Link>
                 </li>
-                <li>
-                    <Link to="/events">Events</Link>
+                <li className='my-2'>
+                    <Link to="/donate">Make a Donation</Link>
+                </li>
+                <li className='my-2'>
+                    <Link to="/membership">Become a member</Link>
                 </li>
             </ul>
             <Outlet/>
         </div>
-        <div>
-            <h2>Contact us</h2>
-            <Media />
-            <div>
-                <h1>Newsletter</h1>
-                <p>Sign up to our Newsletter, to recieve updates on the latest news and upcoming events. We respect ypur privacy, Your Information is safe with us.</p>
-                <form action="">
-                    <input type="email" />
-                    <input type="submit" />
-                </form>
-            </div>
         </div>
     </div>
   )
