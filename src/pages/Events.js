8d8152151f8ff6088de6c11bc3da3f89a7b5e2a2
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-26 15:48:26 +0200

    Add draft style to Events

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -6,33 +6,39 @@ const Events = () => {
     const Upcoming = EventsInfo.filter((item) => item.type === "Upcoming Events");
     const Past = EventsInfo.filter((item) => item.type === "Past Events");  
     return (
-        <div>
-            <section>
-                <h1>Upcoming Events</h1>
-                {Upcoming.map((item)=>{
-                    const {id , title , description} = item;
-                    return(
-                        <div key={id}>
-                            <h2>{title}</h2>
-                            <p>{description}</p>
-                            <Link to={`/events/${id}`} >Learn more</Link>
-                        </div>
-                    )
-                })}
-            </section>
-            <section>
-                <h1>Past Events</h1>
-                {Past.map((item)=>{
-                    const {id , title , description} = item;
-                    return(
-                        <div key={id}>
-                            <h2>{title}</h2>
-                            <p>{description}</p>
-                            <Link to={`/events/${id}`}>Learn more</Link>
-                        </div>
-                    )
-                })}
-            </section>
+        <div className='w-screen h-screen mt-28'>
+            <div className='flex flex-col items-center justify-center'>
+                <section className=''>
+                    <p className='text-5xl text-center my-8'>Upcoming Events</p>
+                    <div className='flex flex-row'>
+                    {Upcoming.map((item)=>{
+                        const {id , title , description} = item;
+                        return(
+                            <div key={id}>
+                                <h2>{title}</h2>
+                                <p>{description}</p>
+                                <Link to={`/events/${id}`} >Learn more</Link>
+                            </div>
+                        )
+                    })}
+                    </div>
+                </section>
+                <section>
+                    <p className='text-5xl text-center my-4'>Past Events</p>
+                    <div className='flex flex-row'>
+                    {Past.map((item)=>{
+                        const {id , title , description} = item;
+                        return(
+                            <div key={id}>
+                                <h2>{title}</h2>
+                                <p>{description}</p>
+                                <Link to={`/events/${id}`}>Learn more</Link>
+                            </div>
+                        )
+                    })}
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
