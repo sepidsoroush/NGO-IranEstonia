f909583afffbc14fa00fd1d73ce6a80f98e8148c
vahid Afshari <vahidinline@gmail.com>
2023-01-27 19:45:13 +0200

    update route

--- src/components/SingleEvent.js
+++ src/components/SingleEvent.js
@@ -2,29 +2,34 @@ import React from 'react';
 import 'react-slideshow-image/dist/styles.css';
 // import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom';
 import EventsInfo from '../data/EventsInfo';
-
+import { Slide } from 'react-slideshow-image';
 
 const SingleEvent = () => {
-    const {id} = useParams();
-    const SelectedEvent = EventsInfo[id -1];
-    console.log(id);
-        return(
-        <div >
-            console.log(id);
-            <h2>{SelectedEvent.title}</h2>
-            <p>{SelectedEvent.description}</p>
-            {/* <Slide>
-                <div className="each-slide-effect">
-                    <div style={{ 'backgroundImage': `url(${SelectedEvent[0]})` , "height" : "50vh" }}>
-                        <span>Slide 1</span>
-                    </div>
-                </div>
-            </Slide> */}
-            <Link to={`/events`}>Back</Link>
+  const { id } = useParams();
+  console.log('event id: ', id);
+  const SelectedEvent = EventsInfo[id - 1];
+  console.log(id);
+  return (
+    <div>
+      console.log(id);
+      <h2>{SelectedEvent.title}</h2>
+      <p>{SelectedEvent.description}</p>
+      <Slide>
+        <div className="each-slide-effect">
+          <div
+            style={{
+              backgroundImage: `url(${SelectedEvent[0]})`,
+              height: '50vh',
+            }}>
+            <span>Slide 1</span>
+          </div>
         </div>
-        );
+      </Slide>
+      <Link to={`/events`}>Back</Link>
+    </div>
+  );
 };
 
 export default SingleEvent;
