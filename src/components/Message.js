99a7557674c7f5582017b5750c8cf57c8fe0c1f0
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-27 18:20:53 +0200

    Add style to Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,14 @@
 const Message = () => {
   return(
     <div>
-        <form action="">
-          <input type="name" placeholder="Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="textarea" placeholder="Your Message" />
-          <input type="submit" value="Send Message" />
+        <form action="" className=" flex flex-col items-center text-sm">
+          <input type="name" placeholder="Name" required className="border border-solid divide-gray-300 w-96 h-10 pl-2 my-1"/>
+          <input type="email" placeholder="Email Address" required
+          className="border border-solid divide-gray-300 w-96 h-10 pl-2 my-1"/>
+          <input type="textarea" placeholder="Your Message" required
+          className="border border-solid divide-gray-300 w-96 h-36 pl-2 my-1"/>
+          <input type="submit" value="Send Message" 
+          className='bg-persian-indigo-800 p-2 mt-3 rounded text-white w-32 text-base'/>
         </form>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Media from "../components/SocialMedia";
 
 const Contact = () => {
   return(
-    <div className="mt-56 flex flex-col justify-center items-center max-w-screen-xl mx-auto">
+    <div className="my-56 flex flex-col justify-center items-center max-w-screen-xl mx-auto">
       <div className="flex flex-col items-center ">
         <h1 className="text-2xl font-semibold mt-8">Feel Free to Contact US</h1>
         <p className="mt-6 mb-3">Info@iraniansupportive.org</p>
@@ -11,7 +11,7 @@ const Contact = () => {
       </div>
       <div className="flex flex-col items-center mt-10">
         <h1 className="text-2xl font-semibold mt-8">Get In Touch</h1>
-        <p className="text-xs mt-1 mb-3">Your email address will not be published. Required fields are marked <span className="text-red-500">*</span></p>
+        <p className="text-xs mt-1 mb-3">Your email address will not be published. All fields are required.</p>
         <Message />
       </div>
     </div>
