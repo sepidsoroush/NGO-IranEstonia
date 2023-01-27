bb5dfb1b23cd1cc81c71396bb3ce684065823efc
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-27 17:28:46 +0200

    Add style to Donate page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,4 +1,4 @@
-import { UilLock } from '@iconscout/react-unicons';
+import { UilLock , UilPaypal , UilGoogle } from '@iconscout/react-unicons';
 import FAQ from "../components/FAQ";
 import FaqInfo from "../data/FaqInfo";
 import {useState } from 'react';
@@ -6,21 +6,30 @@ import {useState } from 'react';
 const Donate = () => {
   const [questions, setQuestions] = useState(FaqInfo);
     return (
-      <div>
-        <h2>Help us do more</h2>
-        <div>
+      <div className='mt-28 max-w-screen-xl flex flex-col mx-auto'>
+        <h1 className='text-5xl py-6 text-center'>Help us do more</h1>
+        <div className='px-10 leading-loose text-lg'>
           <p>Fusce sit amet velit velit. Aliquam id velit malesuada, condimentum.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget velit semper nisl sollicitudin condimentum vitae at felis. Morbi vehicula.</p>
           <p>Pellentesque elementum vel ante sit amet finibus. Donec dapibus ligula eleifend, dictum tortor in, euismod.</p>
         </div>
-        <fieldset>
-          <legend><UilLock/> Secure donation</legend>
-          <a href="https://pay.google.com/">G pay</a>
-          <a href="https://www.paypal.com/">PayPal</a>
-          <a href="https://www.patreon.com/">PATREON</a>
+        <fieldset className='border border-solid border-gray-500 p-3 max-w-screen-sm w-6/12 flex justify-center mx-auto my-10'>
+          <legend> <UilLock className="inline-block"/> <span>Secure donation</span></legend>
+          <div className='flex flex-col font-bold'>
+            <div className='bg-black text-white w-80 h-11 flex items-center justify-center my-1 rounded'>
+              <a href="https://pay.google.com/"><UilGoogle className="inline-block"/> Google Pay</a>
+            </div>
+            <div className='bg-yellow-400 text-white w-80 h-11 flex items-center justify-center my-1 rounded'>
+              <a href="https://www.paypal.com/"><UilPaypal className="inline-block"/> PayPal</a>
+            </div>
+            <div className='bg-red-500 text-white w-80 h-11 flex items-center justify-center my-1 rounded'>
+              <a href="https://www.patreon.com/">PATREON</a>
+            </div>
+          </div>
         </fieldset>
-        <div >
-        <h3>questions and answers about Donation</h3>
+
+        <div className='felx felx-col justify-center max-w-xl mx-auto my-8'>
+          <h3 className='border-t border-gray-200 py-6 text-2xl font-bold text-center'>Questions and Answers about Donation</h3>
           <section >
             {questions.map((question) => {
               return (
