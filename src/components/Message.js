232cc92cd724c6e511e24810a20ea659f6f87b62
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-27 21:12:34 +0200

    update style maessage button

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ const Message = () => {
           <input type="textarea" placeholder="Your Message" required
           className="border border-solid divide-gray-300 w-96 h-36 pl-2 my-1"/>
           <input type="submit" value="Send Message" 
-          className='bg-persian-indigo-800 p-2 mt-3 rounded text-white w-32 text-base'/>
+          className='rounded-full text-persian-indigo-700 border border-solid border-persian-indigo-700 w-34 px-4 py-2 mt-8 text-base'/>
         </form>
     </div>
   );
