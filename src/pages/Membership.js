b08fed2bfa2f68546f9a8cf89129971e50b79898
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-27 21:08:24 +0200

    update style of membership page

--- src/pages/Membership.js
+++ src/pages/Membership.js
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+// JSX for styles
+const inputStyle ="border border-solid border-gray-300 rounded w-72 h-10 pl-2 my-1 ml-3";
+const checkBox ="my-3 mr-3";
+const labelStyle ="text-gray-600";
+
 const Membership = () => {
   const [inputs, setInputs] = useState({});
 
@@ -25,6 +30,7 @@ const Membership = () => {
               value={inputs.userName || ""}
               onChange={handleChange}
               required="required"
+              className={inputStyle}
             />
           </label>
           <label htmlFor="email">Enter your email
@@ -35,40 +41,126 @@ const Membership = () => {
               value={inputs.userEmail || ""}
               onChange={handleChange}
               required="required"
+              className={inputStyle}
             />
           </label>
           
-          <p>Select your filed of activity:</p>
-          <input 
-              type="checkbox" 
-              id='check1'
-              name='rally'
-              value={inputs.rally}
-              onChange={handleChange}
-              />
-          <label htmlFor="check1">Rallies and Events</label>
+          <p className='mb-2 mt-6 font-semibold'>Fields of volunteer collaboration</p>
+          <div>
+            <input 
+                type="checkbox" 
+                id='software'
+                name='software'
+                value={inputs.software}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="software" className={labelStyle}>IT & Software</label>
+          </div>
 
-          <input 
-              type="checkbox" 
-              id='check2'
-              name='media'
-              value={inputs.media}
-              onChange={handleChange}
-              />
-          <label htmlFor="check2">Social Media</label>
+          <div>
+            <input 
+                type="checkbox" 
+                id='translation'
+                name='translation'
+                value={inputs.translation}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="translation" className={labelStyle}>Translation</label>
+          </div>
 
-          <input 
-              type="checkbox" 
-              id='check3'
-              name='financial'
-              value={inputs.financial}
-              onChange={handleChange}
-              />
-          <label htmlFor="check3">Financial Support</label>
-          <input 
-            type="submit"
-            onClick={handleSubmit} 
-          />
+          <div>
+            <input 
+                type="checkbox" 
+                id='editing'
+                name='editing'
+                value={inputs.editing}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="editing" className={labelStyle}>Graphic & Video Editing</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='art'
+                name='art'
+                value={inputs.art}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="art" className={labelStyle}>Art & Culture</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='media'
+                name='media'
+                value={inputs.media}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="media" className={labelStyle}>Social Media</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='legal'
+                name='legal'
+                value={inputs.legal}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="legal" className={labelStyle}>Legal</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='administrative'
+                name='administrative'
+                value={inputs.administrative}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="administrative" className={labelStyle}>Administrative & HR</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='journalism'
+                name='journalism'
+                value={inputs.journalism}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="journalism" className={labelStyle}>Journalism</label>
+          </div>
+
+          <div>
+            <input 
+                type="checkbox" 
+                id='other'
+                name='other'
+                value={inputs.other}
+                onChange={handleChange}
+                className={checkBox}
+                />
+            <label htmlFor="other" className={labelStyle}>Other</label>
+          </div>
+
+          <div className='flex justify-center items-center'>
+            <input 
+              type="submit"
+              onClick={handleSubmit} 
+              className="rounded-full text-persian-indigo-700 border border-solid border-persian-indigo-700  w-24 px-4 py-2 mt-8"
+            />
+          </div>
         </form>
       </div>
     );
