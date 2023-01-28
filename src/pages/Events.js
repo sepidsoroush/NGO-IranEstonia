d13cd55940b1b2e52d1a7a19ec2b3474c3db52ef
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-01-28 17:13:54 +0200

    update style of Events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,43 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import EventsInfo from '../data/EventsInfo';
 
+// JSX for styles
+const titleStyle ="text-4xl font-semibold mb-8 pt-20 text-center";
+const sectionStyle = 'flex flex-wrap flex-row-reverse items-center justify-center ';
+const postStyle = 'w-80 h-96 p-6 m-6 text-center bg-indigo-50 flex flex-col justify-around items-center rounded';
+const postTitle ='text-lg font-bold';
+const linkStyle ='hover:bg-persian-indigo-700 hover:bg-opacity-30 transition duration-500 ease-in-out px-4 py-2 rounded';
+
 const Events = () => {
     const Upcoming = EventsInfo.filter((item) => item.type === "Upcoming Events");
     const Past = EventsInfo.filter((item) => item.type === "Past Events");  
     return (
-        <div className='w-screen h-screen mt-28'>
-            <div className='flex flex-col items-center justify-center'>
-                <section className=''>
-                    <p className='text-5xl text-center my-8'>Upcoming Events</p>
-                    <div className='flex flex-row'>
+        <div className='w-screen mt-28 flex flex-col items-center justify-center'>
+            <section>
+                <h1 className={titleStyle}>Upcoming Events</h1>
+                <div className={sectionStyle}>
                     {Upcoming.map((item)=>{
-                        const {id , title , description} = item;
-                        return(
-                            <div key={id}>
-                                <h2>{title}</h2>
-                                <p>{description}</p>
-                                <Link to={`/events/${id}`} >Learn more</Link>
-                            </div>
-                        )
+                    const {id , title , description} = item;
+                    return(
+                        <div className={postStyle} key={id}>
+                            <h2 className={postTitle}>{title}</h2>
+                            <p>{description}</p>
+                            <Link to={`/events/${id}`} className={linkStyle}>Learn more</Link>
+                        </div>
+                    )
                     })}
-                    </div>
-                </section>
-                <section>
-                    <p className='text-5xl text-center my-4'>Past Events</p>
-                    <div className='flex flex-row'>
+                </div>
+            </section>
+            <section className='mb-8'>
+                <h1 className={titleStyle}>Past Events</h1>
+                <div className={sectionStyle}>
                     {Past.map((item)=>{
-                        const {id , title , description} = item;
-                        return(
-                            <div key={id}>
-                                <h2>{title}</h2>
-                                <p>{description}</p>
-                                <Link to={`/events/${id}`}>Learn more</Link>
-                            </div>
-                        )
+                    const {id , title , description} = item;
+                    return(
+                        <div className={postStyle} key={id}>
+                            <h2 className={postTitle}>{title}</h2>
+                            <p>{description}</p>
+                            <Link to={`/events/${id}`} className={linkStyle} >Learn more</Link>
+                        </div>
+                    )
                     })}
-                    </div>
-                </section>
-            </div>
+                </div>
+            </section>
         </div>
     );
 };
