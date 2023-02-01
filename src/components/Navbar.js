6d2ed45e3e01c752a4b3af1f7c0a4e2db55bc0b3
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-01 21:27:39 +0200

    update Navbar Responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,66 @@
 import { Outlet, Link } from "react-router-dom";
 import Logo from "../data/Images/Logo.jpg";
+import { UilBars , UilMultiply } from '@iconscout/react-unicons';
+import { useState, useRef } from "react";
+
+const links = [
+  {id : 1, url : '/', text : 'Home'},
+  {id : 2, url : '/events', text : 'Events' },
+  {id : 3, url : '/about', text : 'About'},
+  {id : 4, url : '/news', text : 'News'},
+  {id : 5, url : '/membership', text : 'Membership'},
+  {id : 6, url : '/contact', text : 'Contact'},
+  {id : 7 , url : '/donate' , text : 'Donation'}
+];
+
+// JSX for styles
+const navbar ='fixed top-0 z-50 w-screen h-28 box-border text-gray-600 bg-white shadow-lg';
+const navbarStyle = 'md:max-w-6xl md:my-0 md:mx-auto md:flex md:items-center md: justify-between md:p-4';
+const navbarHeader ='md:p-0 flex items-center justify-between p-4 ';
+const logoStyle ='h-20 w-10';
+const titleStyle ='text-md text-persian-indigo-700 font-bold flex-wrap pl-2';
+const logoContainer ='flex items-center justify-center'
+const buttonStyle ='md:hidden';
+const linksContainer ='md:!h-auto  h-0 overflow-hidden transition transition-all duration-300 ease-linear bg-white ';
+const ulLinks = 'md:flex';
+const liLinks = 'md:px-0 md:py-2 md:mx-1 lg:px-1 xl:px-2 sm:py-2 hover:scale-90';
 
 const Navbar = () => {
-  return (
-    <header className="fixed top-0 z-50 w-screen h-28 box-border text-gray-600 bg-white shadow-lg">
-      
-        <div className="flex items-center justify-between my-4 mx-auto max-w-screen-xl ">
-          <a href="/" className="flex flex-row items-center justify-center">
-            <img src={Logo} alt="Iranian supportive organization" className="h-20 w-10 "/>
-            <span className="text-md text-persian-indigo-700 font-bold flex-wrap pl-2">Iranian supportive organization</span>
-          </a>
+  const [showLinks , setShowLinks] =useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () =>{
+    setShowLinks(!showLinks);
+  };
+  const linkStyle = { height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` : '0px' , marginBottom : '5px' , paddingLeft : '10px'};
 
-          <ul className="cursor-pointer flex flex-row items-center justify-end">
-            <li >
-              <Link to="/" className="px-4 py-2">Home</Link>
-            </li>
-            <li>
-              <Link to="/events" className="px-4 py-2">Events</Link>
-            </li>
-            <li>
-              <Link to="/about" className="px-4 py-2">About</Link>
-            </li>
-            <li>
-              <Link to="/membership" className="px-4 py-2">Mambership</Link>
-            </li>
-            <li >
-              <Link to="/contact" className="px-4 py-2">Contact</Link>
-            </li>
-            <li >
-              <Link to="/donate" className="bg-persian-indigo-700 px-4 py-2 rounded-full text-white">Donate</Link>
-            </li>
-          </ul>
+  return (
+    <nav className={navbar}>
+        <div className={navbarStyle}>
+          <div className={navbarHeader}>
+            <a href="/" className={logoContainer}>
+              <img src={Logo} alt="Logo" className={logoStyle}/>
+              <span className={titleStyle}>Iranian supportive organization</span>
+            </a>
+            <button onClick={toggleLinks} className={buttonStyle}>
+              {showLinks? <UilMultiply /> : <UilBars />}
+            </button>
+          </div>
+          <div className={linksContainer} style={linkStyle} ref={linksContainerRef}>
+            <ul className={ulLinks} ref={linksRef}>
+              {links.map((link)=>{
+                const {id,url,text} =link;
+                return(
+                  <li key={id} className={liLinks}>
+                    <Link to={url}>{text}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      
       <Outlet />
-    </header>
+    </nav>
   )
 };
 
