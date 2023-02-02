4b948a2ce26c2ddc5ad3aa99c162d55ef7ab235b
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-02 18:04:57 +0200

    update newsletter form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import {React , useState } from 'react';
 import Media from "./SocialMedia";
 import { Outlet, Link } from "react-router-dom";
+import axios from 'axios';
 
 const Footer = () => {
+  const [newsletter, setNewsletter] = useState({});
+  const [isloading , setIsloading] =useState(false);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setNewsletter(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    setIsloading(true);
+    event.preventDefault();
+    axios.post('http://172.20.10.6:8080/user',newsletter).then(
+      (res)=>{console.log(res);
+        setIsloading(false)}
+    ).catch(
+      (err)=>{console.log(err);
+        setIsloading(false)}
+    );
+  }
+
   return (
     <div className='bg-persian-indigo-50  bg-opacity-50 text-persian-indigo-900 text-sm  w-screen py-6'>
         <div className='flex flex-col md:flex-row  md:justify-around justify-start items-center mx-auto  md:max-w-2xl lg:max-w-3xl px-10'>
@@ -33,11 +55,20 @@ const Footer = () => {
             <p className='text-base font-bold' >Newsletter</p>
             <p className='text-xs text-justify my-2 w-full'>Sign up to our Newsletter, to recieve updates on the latest news and upcoming events. We respect your privacy, Your Information is safe with us.</p>
             <form action="" className='flex flex-row justify-between w-full'>
-                <input type="email" placeholder='Email Address' className=' pl-2 py-1 text-gray-500 mr-2 rounded' />
-                <input type="submit" className='bg-persian-indigo-700 px-2 py-1 rounded text-white'  />
+                <input 
+                type="email"
+                name='userEmail'
+                value={newsletter.userEmail || ""}
+                onChange={handleChange} 
+                placeholder='Email Address' 
+                className=' pl-2 py-1 text-gray-500 mr-2 rounded' />
+                <input 
+                type="submit"
+                value={isloading ? 'Submiting...' : 'Submit'}
+                onClick={handleSubmit} 
+                className='bg-persian-indigo-700 px-2 py-1 rounded text-white'  />
             </form>
         </div>
-        
         </div>
     </div>
   )
