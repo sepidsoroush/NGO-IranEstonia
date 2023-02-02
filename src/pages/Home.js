9c1af9198ed35a63bb3f86d3672b3ebcfa55efd8
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-02 21:14:07 +0200

    update Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,16 @@ import  {AboutUs, OurStory } from '../components/AboutUs';
 const Home = () => {
     return(
       <div>
-        <section className=' mt-28 '>
+        <section className='mt-28 '>
           <AboutUs/>
           <OurStory />
         </section>
-        <section>
+        <section className='flex flex-col justify-center items-center'>
           <UpcomingEvent />
+          <a href="./Events" className='bg-persian-indigo-700 text-white rounded px-4 py-2'>About Previous Events</a>
         </section>
         <section className=' my-20 '>
-          <h1 className="text-4xl font-semibold mb-8 pt-20 text-center">Lets Create Something Great Together!</h1>
+          <h1 className="text-3xl font-semibold mb-8 pt-20 text-center">Lets Create Something Great Together!</h1>
           <Message />
         </section>
       </div>
