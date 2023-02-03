d1912b25a7c6074719cdae54a202607bbef9f351
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-03 20:23:51 +0200

    update our story info

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -18,8 +18,8 @@ const AboutUs = () => {
     return(
       <div>
         <h2 className={titleStyle} >Our Story</h2>
-        <p className={textStyle} >Sed porttitor lectus nibh. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
-        <p className={textStyle} >Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.</p>
+        <p className={textStyle} >Since the brutal death of Mahsa Amini in September 2022 and before the establishment of the NPO, the members of the Iranian Supportive Organization have also been active in social media, using platforms like Twitter, Facebook, and Instagram to reach a wider audience. They have been posting regular updates and stories about the situation in Iran, providing a firsthand account of what is happening on the ground. They have also been working with each other to create a network of support and advocacy for the Iranian people in a campaign called “Free Iran”.</p>
+        <p className={textStyle} >We were committed to giving a voice to the people of Iran. We felt that the Iranian people were not being heard and their voices were not being represented in the international arena, and we have been actively participating in various activities to raise awareness and bring attention to the cause.</p>
       </div>
     );
     };  
