1fed20eee0cef248e88eac5cbc8c3437c0c214f6
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-03 14:26:18 +0200

    update FAQ info

--- src/data/FaqInfo.js
+++ src/data/FaqInfo.js
@@ -1,33 +1,51 @@
 const questions = [
     {
       id: 1,
-      title: 'How can I get help with my donation?',
+      title: 'What is the mission and vision of the NGO?',
       info:
-        'Fusce sit amet velit velit. Aliquam id velit malesuada, condimentum.',
+        'Our mission is to support and promote the activities of the Iranian community in Estonia and abroad. We aim to provide opportunities for members of the community to participate in cultural, and political? and social activities that benefit them and bring them together.'
     },
     {
       id: 2,
-      title: 'How transprant is ISO?',
+      title: 'How will the donations be used to support the cause?',
       info:
-        'Fusce sit amet velit velit. Aliquam id velit malesuada, condimentum.',
+        'Donations will be used to fund the various activities and programs that we organize to achieve our mission and vision. This includes cultural events, political discussions, social gatherings, and other initiatives that bring the community together and help it thrive.'
     },
     {
       id: 3,
-      title: 'How can I make a one-time donation?',
+      title: 'Is the donation tax-deductible?',
       info:
-        'Pellentesque elementum vel ante sit amet finibus. Donec dapibus ligula eleifend, dictum tortor in, euismod.',
+        'We are not sure about the tax deductibility of donations at this time. We recommend checking with a local tax professional or relevant government agency to find out if your donation is tax-deductible.'
     },
     {
       id: 4,
-      title: 'Can I mail a pysical check?',
+      title: 'What are the different ways to donate (online, by check, recurring donation, etc.)?',
       info:
-        'Fusce sit amet velit velit. Aliquam id velit malesuada, condimentum.',
+        'Donations can be made online via our website or by bank transfer. Recurring donations are also an option for those who would like to make a long-term commitment to our cause.'
     },
     {
       id: 5,
-      title: 'I set up a monthly donation, but I need to update or pause the monthly recurrence. How can I do this?',
+      title: 'How can I track the impact of my donation?',
       info:
-        'Pellentesque elementum vel ante sit amet finibus. Donec dapibus ligula eleifend, dictum tortor in, euismod.',
+        'We will provide regular updates on the progress of our programs and initiatives, as well as the impact of individual donations. This information will be available on our website and through other communication channels.'
+    },
+    {
+      id: 6,
+      title: "How can I receive updates on the NGO's work and progress?",
+      info:
+        'You can sign up for our email newsletter or follow us on social media to receive updates on our work and progress.'
+    },
+    {
+      id: 7,
+      title: "How can I get in touch with the NGO for further questions or to learn more?",
+      info:
+        'You can contact us through our website or by sending an email. We are always happy to hear from members of the community and answer any questions you may have.'
+    },
+    {
+      id: 8,
+      title: "How is my personal information protected?",
+      info:
+        'We take the protection of your personal information very seriously. All information collected through donations or membership/volunteer applications will be kept confidential and will not be shared with third parties. We use secure systems and processes to protect your data, and we comply with all relevant privacy laws and regulations. If you have any concerns about the protection of your personal information, please feel free to get in touch with us.'
     },
   ]
   export default questions;
