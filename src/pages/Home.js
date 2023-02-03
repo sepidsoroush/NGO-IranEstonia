a1b9d9a8b2570321f4a64de80b833b794cb32f57
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-03 16:28:05 +0200

    Add parallax section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
 import UpcomingEvent from '../components/UpcomingEvent' ;
 import Message from '../components/Message';
 import  {AboutUs, OurStory } from '../components/AboutUs';
+import Hero from '../data/Images/Hero.jpeg';
+
 
 const Home = () => {
     return(
-      <div className='md:max-w-6xl mx-auto'>
-        <section className='mt-28 '>
+      <div className=''>
+        <section className='flex items-center justify-center h-screen w-screen bg-fixed bg-center bg-cover overflow-x-hidden mt-28' style={{backgroundImage : `url('${Hero}')`}}>
+          <div className='p-5 text-2xl text-persian-indigo-900 bg-white bg-opacity-50 rounded'>
+            Welcome to ISO Website
+          </div>
+        </section>
+        <section className='md:max-w-6xl mx-auto'>
           <AboutUs/>
           <OurStory />
         </section>
-        <section className='flex flex-col justify-center items-center'>
+        <section className='flex flex-col justify-center items-center md:max-w-6xl mx-auto'>
           <UpcomingEvent />
-          <a href="./Events" className='bg-persian-indigo-700 text-white rounded px-4 py-2'>About Previous Events</a>
         </section>
-        <section className=' my-20 '>
+        <section className=' my-20 md:max-w-6xl mx-auto'>
           <h1 className="text-3xl font-semibold mb-8 pt-20 text-center">Lets Create Something Great Together!</h1>
           <Message />
         </section>
