864bb395cff5f520797c5308486f9f86c7850df7
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-02-13 10:51:47 +0200

    add show more/less button

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import {React , useState} from 'react';
 
 // JSX for styles
 const textStyle =" px-10";
 const titleStyle ="text-4xl font-semibold mb-8 pt-20 text-center";
-
+const btn = 'pl-2 text-persian-indigo-600'
 const AboutUs = () => {
+  const [showMore, setShowMore] = useState(false);
+  const text = 'The Iranian Supportive Organization (ISO) is a newly formed NGO based in Estonia dedicated to promoting the Iranian culture and supporting Iranians, especially newcomers and students, who reside in Estonia. ISO recognizes the challenges and is committed to providing resources and support to help Iranians integrate into Estonian society. Through a range of programs and services including social and cultural events, ISO aims to create a sense of community and belonging among Iranians in Estonia. The formation of ISO marks a significant step forward in promoting cultural exchange and understanding between different groups in Estonian society. By fostering a supportive environment for Iranians, ISO hopes to bridge cultural gaps and create a more inclusive community for all. We believe that the Iranian community in Estonia will be strengthened even further by the formation of ISO and the positive impact it will have on the lives of individuals.'
   return(
     <div>
       <h2 className={titleStyle} >About Iranian supportive organization (ISO)</h2>
-        <p className={textStyle} >The Iranian Supportive Organization (ISO) is a newly formed NGO based in Estonia dedicated to promoting the Iranian culture and supporting Iranians, especially newcomers and students, who reside in Estonia. ISO recognizes the challenges and is committed to providing resources and support to help Iranians integrate into Estonian society. Through a range of programs and services including social and cultural events, ISO aims to create a sense of community and belonging among Iranians in Estonia. <br /> The formation of ISO marks a significant step forward in promoting cultural exchange and understanding between different groups in Estonian society. By fostering a supportive environment for Iranians, ISO hopes to bridge cultural gaps and create a more inclusive community for all. We believe that the Iranian community in Estonia will be strengthened even further by the formation of ISO and the positive impact it will have on the lives of individuals.</p>
+        <h6 className={textStyle}>
+          {showMore ? text : `${text.substring(0, 250).concat('...')}`}
+          <button className={btn} onClick={() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
+        </h6>
     </div>
   );
   };
   const OurStory = () => {
+    const [showMore, setShowMore] = useState(false);
+    const text = 'We are a diverse group of independent individuals who came together with a shared vision of improving the lives of Iranians in Estonia. We are not affiliated with any political party or specific group, and our mission is driven purely by our desire to support our community. Through our interactions with the Iranian community in Estonia, we realized that there was a lack of representation and support for Iranians. Despite the numerous resources and services provided by the country to help newcomers settle, it often takes months for individuals to find and access these resources.  In response to this challenge, we decided to form an NGO dedicated to promoting Iranian culture and supporting Iranians. Our goal is to provide a comprehensive range of programs and support services that can help Iranians integrate into Estonian society and improve their quality of life. We are excited about the potential impact of ISO and are committed to working tirelessly to achieve our goals. We hope that through our efforts, we can create a more inclusive and supportive environment for Iranians in Estonia.'
+
     return(
       <div>
         <h2 className={titleStyle} >Our Story</h2>
-        <p className={textStyle} >We are a diverse group of independent individuals who came together with a shared vision of improving the lives of Iranians in Estonia. We are not affiliated with any political party or specific group, and our mission is driven purely by our desire to support our community. <br/> Through our interactions with the Iranian community in Estonia, we realized that there was a lack of representation and support for Iranians. Despite the numerous resources and services provided by the country to help newcomers settle, it often takes months for individuals to find and access these resources. <br/> In response to this challenge, we decided to form an NGO dedicated to promoting Iranian culture and supporting Iranians. Our goal is to provide a comprehensive range of programs and support services that can help Iranians integrate into Estonian society and improve their quality of life. <br/> We are excited about the potential impact of ISO and are committed to working tirelessly to achieve our goals. We hope that through our efforts, we can create a more inclusive and supportive environment for Iranians in Estonia.</p>
+        <h6 className={textStyle}>
+          {showMore ? text : `${text.substring(0, 250).concat('...')}`}
+          <button className={btn} onClick={() => setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"}</button>
+        </h6>
       </div>
     );
     };  
